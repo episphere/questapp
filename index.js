f607a42c13175febbdcbe7cb42938d8859415395
Ruggieri <ruggierine@nci-01978419-ml.fios-router.home>
2020-06-05 12:15:29 -0400

    changed questURL

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { transform } from 'https://episphere.github.io/quest/replace2.js';
 
-let questURL = location.href
+let questURL = location.search.replace(/[\?]/g, "")
 
 window.onload = async() => {
     await transform.render({
