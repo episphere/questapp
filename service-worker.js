fe4c7373516b801637c64fdd983be17a71c5f4f8
Ruggieri <ruggierine@nci-01978419-ml.fios-router.home>
2020-06-08 19:21:17 -0400

    added home button

--- service-worker.js
+++ service-worker.js
@@ -29,5 +29,5 @@ registerRoute(
     'GET'
 );
 
-workbox.precaching.precacheAndRoute([{url: 'index.html', revision: '4'}]);
+workbox.precaching.precacheAndRoute([{url: 'index.html', revision: 'v5'}]);
 // workbox.precaching.precacheAndRoute([{url: 'index.html', revision: `${new Date().getTime()}`}]);
